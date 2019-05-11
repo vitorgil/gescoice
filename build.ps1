1c953ed61fc4f8f5207ef8bf1380cfb82a133863
Vítor Pinheiro <vitorgilvitorino@gmail.com>
2019-05-11 23:21:18 +0200

    Move windows specific commands to build script

--- build.ps1
+++ build.ps1
@@ -1,4 +1,20 @@
-$env:Path += ";C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin"
+Set-Variable -Name "install_path"  -Value "C:/projects/coice/install"
+Set-Variable -Name "toolchain_bin" -Value "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64i/bin"
+Set-Variable -Name "qt_path"       -Value "C:/Qt/5.12.3/mingw73_64"
 
-cmake .. -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX="c:/projects/coice/install"
+$env:Path += ";$toolchain_bin"
+
+cmake .. -G "MinGW Makefiles" `
+         -DCMAKE_INSTALL_PREFIX="$install_path" `
+         -DCMAKE_PREFIX_PATH="$qt_path/lib/cmake/Qt5" `
+         -DGTEST_ROOT="C:/googletest-distribution"
+
 mingw32-make -j5 install
+
+# Copy because these are not on the path
+Copy-Item "$qt_path/bin/Qt5Widgets.dll" -Destination $install_path/bin/
+Copy-Item "$qt_path/bin/Qt5Core.dll"    -Destination $install_path/bin/
+Copy-Item "$qt_path/bin/Qt5Gui.dll"     -Destination $install_path/bin/
+Copy-Item "$toolchain_bin/libgcc_s_seh-1.dll"  -Destination $install_path/bin/
+Copy-Item "$toolchain_bin/libstdc++-6.dll"     -Destination $install_path/bin/
+Copy-Item "$toolchain_bin/libwinpthread-1.dll" -Destination $install_path/bin/
